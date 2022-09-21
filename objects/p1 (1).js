d1a838774b076e251b84d3f955edf07684b678c4
sandeepyenugupalli <sandeepnarayana0156@gmail.com>
2022-09-21 21:49:49 +0530

    objects problems updated

---
+++ objects/p1 (1).js
@@ -0,0 +1,16 @@
+// ["Nrupul", "Prateek", "Aman", "Albert", "Yogesh"]
+// [32, 30, 26, 28, 44]
+let bag="";
+let details=[
+  {name:'Nrupul',age:32},
+  {name:'prateek',age:30},
+  {name:'Aman',age:26},
+  {name:'Albert',age:28},
+  {name:'Yogesh',age:44}
+];
+for(i=0; i<=details.length-1; i++){
+if(details[i]["age"]>30){
+  bag=details[i]["name"]+" "+bag;
+}
+}
+console.log(bag);
---
+++ objects/p2 (1).js
@@ -0,0 +1,29 @@
+let arr=[
+	{
+		grade: "V",
+		students: [
+			{name: "Nrupul", marks: [10, 20, 30]},
+			{name: "Prateek", marks: [20, 30, 40]}
+		]
+	},
+	{
+		grade: "VI",
+		students: [
+			{name: "Aman", marks: [10, 20, 30]},
+			{name: "Albert", marks: [20, 30, 40]}
+		]
+	},
+	{
+		grade: "VII",
+		students: [
+			{name: "Yogesh", marks: [10, 20, 30]},
+			{name: "Sandhya", marks: [20, 30, 40]}
+		]
+	}
+]
+let bag1=0;
+for(i=0; i<=arr.length-1; i++){
+  let sum=arr[i]["marks"[i]];
+  bag1=bag1+sum;
+}
+console.log(bag1);
